4d80f3c92e1459348ecf6f4c33ae89ff2e6595ab
VioletViola <vivian.v18723@gmail.com>
2021-08-17 19:02:33 +0100

    Refactor code.

--- src/ScrabbleScore.java
+++ src/ScrabbleScore.java
@@ -2,7 +2,7 @@ public class ScrabbleScore {
     final static String[] ALL_LETTERS = {"AEIOULNRST", "DG", "BCMP", "FHVWY", "K", "JX", "QZ"};
     final static int[] ALL_POINTS = {1, 2, 3, 4, 5, 8, 10};
 
-    public int getScrabbleScore (String word) {
+    public int getScrabbleScore(String word) {
         int wordScore = 0;
             for (int i = 0; i < ALL_LETTERS.length; i++) {
                 for (char singleWordLetter : word.toUpperCase().toCharArray()) {
